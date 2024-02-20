bc10ae134e723026d1a8cacf00ad05b5fe2d7278
ji-jjang <jinyhehe@gmail.com>
2024-02-20 11:52:11 +0900

    chore: add docker-compose, application.yml, mapstruct util

---
+++ src/test/java/com/juny/spacestory/SpacestoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juny.spacestory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacestoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
