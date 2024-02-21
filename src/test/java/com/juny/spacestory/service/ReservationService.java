c07982a30457f242f8bec0e483129d7f7630a546
ji-jjang <jinyhehe@gmail.com>
2024-02-21 23:06:50 +0900

    feat: test domain mapping

---
+++ src/test/java/com/juny/spacestory/service/ReservationService.java
@@ -0,0 +1,10 @@
+package com.juny.spacestory.service;
+
+import com.juny.spacestory.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+}
