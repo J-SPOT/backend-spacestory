4f22d840a0f4b5929c39948cf9c8775c81a6a185
jinyus <jinyhehe@gmail.com>
2024-07-10 22:31:42 +0900

    feat: 카테고리 이름으로 검색 기능 추가

--- src/main/java/com/juny/spacestory/space/domain/category/ResSubCategory.java
+++ src/main/java/com/juny/spacestory/space/domain/category/ResSubCategory.java
@@ -1,5 +1,5 @@
 package com.juny.spacestory.space.domain.category;
 
-public record ResSubCategory(Long id, String name) {
+public record ResSubCategory(Long id, String name, ResMainCategory mainCategory) {
 
 }
