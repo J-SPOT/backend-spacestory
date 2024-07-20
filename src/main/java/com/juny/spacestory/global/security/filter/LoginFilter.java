039090231a8dd4f837c29331591675089a2fd92a
jinyus <jinyhehe@gmail.com>
2024-07-20 12:16:17 +0900

    feat: 공용 IP 추적하는 로그 테스트

--- src/main/java/com/juny/spacestory/global/security/filter/LoginFilter.java
+++ src/main/java/com/juny/spacestory/global/security/filter/LoginFilter.java
@@ -118,6 +118,33 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     List<String> ipAddresses = customUserDetails.getIpAddresses();
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
+    String ip = request.getHeader("X-Forwarded-For");
+    log.info("X-Forwarded-For: {}", ip);
+    if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("Proxy-Client-IP");
+      log.info("Proxy-Client-IP: {}", ip);
+    }
+    if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("WL-Proxy-Client-IP");
+      log.info("WL-Proxy-Client-IP: {}", ip);
+    }
+    if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("HTTP_CLIENT_IP");
+      log.info("HTTP_CLIENT_IP: {}", ip);
+    }
+    if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+      log.info("HTTP_X_FORWARDED_FOR: {}", ip);
+    }
+    if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getRemoteAddr();
+      log.info("REMOTE_ADDR: {}", ip);
+    }
+
+    if (ip != null && ip.contains(",")) {
+      ip = ip.split(",")[0];
+    }
+    log.info("ip: {}", ip);
 
     if (ipAddresses.contains(request.getRemoteAddr()) && !isTotpEnabled) {
       issueTokens(response, id, role);
