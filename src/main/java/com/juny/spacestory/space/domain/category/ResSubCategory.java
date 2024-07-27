505b0360b6146b134906ffbedadd49142b6d0bed
jinyus <jinyhehe@gmail.com>
2024-07-27 16:56:46 +0900

    feat: 사용하지 않는 것 삭제, ResSubCategory 더 단순한 DTO 구조로 수정

--- src/main/java/com/juny/spacestory/space/domain/category/ResSubCategory.java
+++ src/main/java/com/juny/spacestory/space/domain/category/ResSubCategory.java
@@ -1,5 +1,7 @@
 package com.juny.spacestory.space.domain.category;
 
-public record ResSubCategory(Long id, String name, ResMainCategory mainCategory) {
-
+public record ResSubCategory(Long id, String name, Long mainCategoryId, String mainCategoryName) {
+  public ResMainCategory mainCategory() {
+    return new ResMainCategory(mainCategoryId, mainCategoryName);
+  }
 }
