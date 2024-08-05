29db9798fbd3dcb3e1644c2000dcd1f79413136f
jinyus <jinyhehe@gmail.com>
2024-08-05 19:15:32 +0900

    feat: 부동산 엔티티 soft delete 추가, 승인 및 취소 로직 추가

--- src/main/java/com/juny/spacestory/space/domain/realestate/RealEstate.java
+++ src/main/java/com/juny/spacestory/space/domain/realestate/RealEstate.java
@@ -27,17 +27,23 @@ public class RealEstate {
   @Column(nullable = false)
   private Boolean hasElevator;
 
+  @Column
   private LocalDateTime deletedAt;
+
+  @Column
+  @Enumerated(EnumType.STRING)
+  private RealEstateStatus status;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
   private User user;
 
-  public RealEstate(Address address, Integer floor, Boolean hasParking, Boolean hasElevator) {
+  public RealEstate(Address address, Integer floor, Boolean hasParking, Boolean hasElevator, RealEstateStatus status) {
     this.address = address;
     this.floor = floor;
     this.hasParking = hasParking;
     this.hasElevator = hasElevator;
+    this.status = status;
   }
 
   // 연관관계 편의 메서드
@@ -45,10 +51,23 @@ public class RealEstate {
     this.user = user;
   }
 
-  public void updateRealEstateForm(ReqRealEstate req) {
+  public void updateRealEstateForm(ReqCreateRealEstateByAdmin req, RealEstateStatus status) {
     this.address = req.address();
     this.floor = req.floor();
     this.hasParking = req.hasParking();
     this.hasElevator = req.hasElevator();
+    this.status = status;
+  }
+
+  public void approveRealEstate() {
+    this.status = RealEstateStatus.승인;
+  }
+
+  public void cancelRealEstate() {
+    this.status = RealEstateStatus.취소;
+  }
+
+  public void deleteSoft() {
+    this.deletedAt = LocalDateTime.now();
   }
 }
